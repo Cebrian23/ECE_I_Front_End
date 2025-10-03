8fadf58278e10ae9598ef40703e6dbed81226dfc
Miguel <miguelangel5cebrian@gmail.com>
2025-10-04 01:22:10 +0200

    Inserción de nuevas rutas

---
+++ routes/festivity/[id].tsx
@@ -0,0 +1,39 @@
+import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
+import { FestivityDB } from "../../types/festivity/Festivity.ts";
+
+type Data = {
+    festivity?: FestivityDB,
+}
+
+const handler: Handlers<Data> = {
+    GET: async (req: Request, ctx: FreshContext<unknown, Data>) =>{
+        const id = ctx.params.id;
+
+        //
+        
+        return ctx.render({});
+    }
+}
+
+const Page = (props: PageProps<Data>) => {
+    const festivity = props.data.festivity;
+
+    return(
+        <div>
+            {
+                festivity !== undefined &&
+                <div>
+                    <div>
+                        <h1>Página de {festivity.name}</h1>
+                    </div>
+                    <div>
+                        <p><b>Nombre: </b>{festivity.name}</p>
+                        <p><b>Fecha: </b>{festivity.date.day + " de " + festivity.date.month}</p>
+                    </div>
+                </div>
+            }
+        </div>
+    );
+}
+
+export default Page;
---
+++ routes/festivity/festivities.tsx
@@ -0,0 +1,54 @@
+import { Handlers, FreshContext, PageProps } from "$fresh/server.ts";
+import Festivities from "../../islands/Festivities.tsx";
+import { FestivityDB } from "../../types/festivity/Festivity.ts";
+
+type Data = {
+    festivities?: FestivityDB[],
+}
+
+export const handler: Handlers<Data> = {
+    GET: async (req: Request, ctx: FreshContext<unknown, Data>) => {
+        const url = new URL(req.url);
+
+        const name = url.searchParams.get("name");
+        if(name){
+            //
+        }
+        else{
+            const month = url.searchParams.get("month_a");
+            const day = url.searchParams.get("day_a");
+
+            if(month && day){
+                //
+            }
+            else{
+                return ctx.render({});
+            }
+        }
+
+        return ctx.render({});
+    }
+}
+
+const Page = (props: PageProps<Data>) => {
+    const festivities = props.data.festivities;
+
+    return(
+        <div>
+            {
+                festivities === undefined &&
+                <Festivities/>
+            }
+            {
+                festivities !== undefined &&
+                festivities.map((festivity) => {
+                    <div>
+                        <p>{festivity.name}</p>
+                    </div>
+                })
+            }
+        </div>
+    );
+}
+
+export default Page;
