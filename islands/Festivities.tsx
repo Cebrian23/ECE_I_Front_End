1417efc0243db43f95f36a66f45410106a85376c
Miguel <miguelangel5cebrian@gmail.com>
2025-10-04 01:25:26 +0200

    Inserción del filtro de canciones de festividades

---
+++ islands/Festivities.tsx
@@ -0,0 +1,41 @@
+import { useState } from "preact/hooks";
+import NameForm from "./NameForm.tsx";
+import DateForm from "./DateForm.tsx";
+
+const Festivities = () => {
+    const [initialForm, showInitialForm] = useState<boolean>(true);
+    const [nameFilter, showNameFilter] = useState<boolean>(false);
+    const [dateFilter, showInitDateFilter] = useState<boolean>(false);
+
+    return(
+        <div>
+            {
+                initialForm.valueOf() === true &&
+                <form>
+                    <h1>Selecciona la acción con la que desea obtener los datos:</h1>
+                    <br/>
+                    <button type="button" onClick={() => {
+                        showInitialForm(false);
+                        showNameFilter(true);
+                    }}>Filtrar por nombre</button>
+                    <br/>
+                    <button type="button" onClick={() => {
+                        showInitialForm(false);
+                        showInitDateFilter(true);
+                    }}>Filtrar por fecha</button>
+                    <br/>
+                </form>
+            }
+            {
+                nameFilter.valueOf() === true &&
+                <NameForm surname={false} page_back="Festivities"/>
+            }
+            {
+                dateFilter.valueOf() === true &&
+                <DateForm doubleFilter={false} type="Festivity" page_back="Festivities"/>
+            }
+        </div>
+    );
+}
+
+export default Festivities;
