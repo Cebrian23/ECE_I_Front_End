6f67b18b4c81adeaeed5c51066f9dc60ac8d2fc1
Miguel <miguelangel5cebrian@gmail.com>
2025-10-08 00:44:54 +0200

    Actualización del tipo Organization

--- types/history/Organization.ts
+++ types/history/Organization.ts
@@ -1,7 +1,6 @@
 import { ObjectId, OptionalId } from "mongodb";
 import { Date } from "./Date.ts";
-import { Person, PersonDB } from "./Person.ts";
-import { EventDB } from "./Event.ts";
+import { Person } from "./Person.ts";
 
 export type OrganizationDB = OptionalId<{
     name: string,
@@ -9,18 +8,7 @@ export type OrganizationDB = OptionalId<{
     creation: Date,
     dissolution?: Date,
     distinguished_members?: ObjectId[],
-    involved_in?: ObjectId[],
 }>
-
-export type OrganizationGQL = {
-    id: string,
-    name: string,
-    logo?: string,
-    creation: Date,
-    dissolution?: Date,
-    distinguished_members?: PersonDB[],
-    involved_in?: EventDB[],
-}
 
 export type Organization = {
     id: string,
