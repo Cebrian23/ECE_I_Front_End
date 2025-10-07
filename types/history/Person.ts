003ad8b655163941b221600f363c95feb983a71a
Miguel <miguelangel5cebrian@gmail.com>
2025-10-08 00:45:17 +0200

    Actualización del tipo Person

--- types/history/Person.ts
+++ types/history/Person.ts
@@ -3,29 +3,23 @@ import { Date } from "./Date.ts";
 
 export type PersonDB = OptionalId<{
     name: string,
-    surname: string,
+    surname?: string,
     nickname?: string[],
     image?: string,
     birth_date: Date,
     death_date?: Date,
+    country_from: string,
+    historical_position: string,
 }>
-
-export type PersonGQL = {
-    id: string,
-    name: string,
-    surname: string,
-    nickname?: string[],
-    image?: string,
-    birth_date: Date,
-    death_date?: Date,
-};
 
 export type Person = {
     id: string,
     name: string,
-    surname: string,
+    surname?: string,
     nickname?: string[],
     image?: string,
     birth_date: Date,
     death_date?: Date,
+    country_from: string,
+    historical_position: string,
 }
