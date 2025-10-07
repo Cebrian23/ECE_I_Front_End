88bd00c4f87c0fa166c10f4d7e434f6362918261
Miguel <miguelangel5cebrian@gmail.com>
2025-10-08 00:35:09 +0200

    Actualización del Header

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,13 +1,16 @@
 const Head = () =>  {
     return(
         <div class="head">
+            <a href="/">Home</a>
             <a href="/event/events">Eventos Históricos</a>
             <a href="/person/people">Personas Históricas</a>
             <a href="/organization/organizations">Organizaciones Históricas</a>
             <a href="/book/literature">Literatura</a>
             <a href="/heraldry/heraldries">Heráldica</a>
             <a href="/legend/legends">Leyendas</a>
+            <a href="/mith/miths">Mitos</a>
             <a href="/festivity/festivities">Festividades</a>
+            <a href="/monument/monuments">Monumentos</a>
         </div>
         
     );
