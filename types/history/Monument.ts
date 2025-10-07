8a5bec34cda8885df73b0bb44e0fca61a1788dc7
Miguel <miguelangel5cebrian@gmail.com>
2025-10-08 00:45:57 +0200

    Inserción del tipo Monument

---
+++ types/history/Monument.ts
@@ -0,0 +1,17 @@
+import { OptionalId } from "mongodb";
+import { Date_monument } from "./Date.ts";
+
+export type MonumentDB = OptionalId<{
+    name: string,
+    creation?: Date_monument,
+    destruction?: Date_monument,
+    country_in: string,
+}>
+
+export type Monument = {
+    id: string,
+    name: string,
+    creation?: Date_monument,
+    destruction?: Date_monument,
+    country_in: string,
+}
