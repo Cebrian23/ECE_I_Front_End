e1359946ce66dbf9acac75aef55c0de1d862fbed
Miguel <miguelangel5cebrian@gmail.com>
2025-10-08 18:36:16 +0200

    Inserción de filtros de canciones

---
+++ islands/Miths.tsx
@@ -0,0 +1,34 @@
+import { useState } from "preact/hooks";
+import NameForm from "./NameForm.tsx";
+
+const Miths = () => {
+    const [initialForm, showInitialForm] = useState<boolean>(true);
+    const [nameFilter, showNameFilter] = useState<boolean>(false);
+    
+    return(
+        <div>
+            {
+                initialForm.valueOf() === true &&
+                <form>
+                    <h1>Selecciona la acción con la que desea obtener los datos:</h1>
+                    <br/>
+                    <button type="button" onClick={() => {
+                        showInitialForm(false);
+                        showNameFilter(true);
+                    }}>Filtrar por nombre</button>
+                    <br/>
+                    <button type="button" onClick={() => {
+                        showInitialForm(false);
+                    }}>Mostrar todos los datos</button>
+                    <br/>
+                </form>
+            }
+            {
+                nameFilter.valueOf() === true &&
+                <NameForm surname={false} page_back="Literature"/>
+            }
+        </div>
+    );
+}
+
+export default Miths;
