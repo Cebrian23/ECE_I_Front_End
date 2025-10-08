27d5c2f062058030430ef8db065e3b7cf8b9dd90
Miguel <miguelangel5cebrian@gmail.com>
2025-10-08 17:33:48 +0200

    Actualización del tipo Mith

--- types/legend/Mith.ts
+++ types/legend/Mith.ts
@@ -4,6 +4,15 @@ export type MithDB = OptionalId<{
     name: string,
 }>
 
+export type MithGQL = {
+    id: string,
+    name: string,
+    talked_about_in: {
+        id: string,
+        name: string,
+    }
+}
+
 export type Mith = {
     id: string,
     name: string,
