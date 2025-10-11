d4e2d9ee135295f1cfb006a3d48c04663bb1bc41
Miguel <miguelangel5cebrian@gmail.com>
2025-10-11 22:00:36 +0200

    Actualización del tipo Festivity

--- types/festivity/Festivity.ts
+++ types/festivity/Festivity.ts
@@ -6,6 +6,21 @@ export type FestivityDB = OptionalId<{
     date: Festivity_date,
 }>
 
+export type FestivityGQL = {
+    id: string,
+    name: string,
+    date: Festivity_date,
+    talked_about_in: {
+        id: string,
+        name: string,
+        cover: string
+        album_in: {
+            id: string,
+            name: string,
+        }
+    }[]
+}
+
 export type Festivity = {
     id: string,
     name: string,
