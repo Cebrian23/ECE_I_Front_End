aadf32eece388bde11fefdd9f841f005ba9836b2
Miguel <miguelangel5cebrian@gmail.com>
2025-10-11 22:05:40 +0200

    Actualización del tipo Book

--- types/literature/Book.ts
+++ types/literature/Book.ts
@@ -10,9 +10,18 @@ export type BookDB = OptionalId<{
 export type BookGQL = {
     id: string,
     title: string,
-    cover?: string,
-    year_of_publish?: number,
-    description?: string,
+    cover: string | null,
+    year_of_publish: number | null,
+    description: string | null,
+    talked_about_in: {
+        id: string,
+        name: string,
+        cover: string
+        album_in: {
+            id: string,
+            name: string,
+        }
+    }[]
 }
 
 export type Book = {
