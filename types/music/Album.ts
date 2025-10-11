0e553199655e7f8e8ba38bb3b9c66d567e1e3a0b
Miguel <miguelangel5cebrian@gmail.com>
2025-10-11 22:14:52 +0200

    Actualización del tipo Album

--- types/music/Album.ts
+++ types/music/Album.ts
@@ -1,5 +1,5 @@
 import { ObjectId, OptionalId } from "mongodb";
-import { Song, SongDB } from "./Song.ts";
+import { Song } from "./Song.ts";
 import { Topics } from "./Topics.ts";
 
 export type AlbumDB = OptionalId<{
@@ -13,9 +13,49 @@ export type AlbumDB = OptionalId<{
 export type AlbumGQL = {
     name: string,
     year_of_publish: number,
-    cover?: string,
-    songs: SongDB[],
-    talk_about?: Topics[],
+    cover: string | null,
+    songs: {
+        id: string,
+        name: string,
+    }[] | null,
+    talk_about: {
+        events: {
+            id: string,
+            name: string,
+        }[],
+        organizations: {
+            id: string,
+            name: string,
+        }[],
+        people: {
+            id: string,
+            name: string,
+        }[],
+        books: {
+            id: string,
+            name: string,
+        }[],
+        heraldries: {
+            id: string,
+            name: string,
+        }[],
+        legends: {
+            id: string,
+            name: string,
+        }[],
+        miths: {
+            id: string,
+            name: string,
+        }[],
+        festivities: {
+            id: string,
+            name: string,
+        }[],
+        monuments: {
+            id: string,
+            name: string,
+        }[],
+    } | null,
     conceptual_album: boolean,
 }
 
