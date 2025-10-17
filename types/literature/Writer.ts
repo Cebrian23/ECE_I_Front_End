9ea073adc460c0dff060f5aea1216138bd40786c
Miguel <miguelangel5cebrian@gmail.com>
2025-10-17 19:09:18 +0200

    Actualización del tipo Writer

--- types/literature/Writer.ts
+++ types/literature/Writer.ts
@@ -3,8 +3,8 @@ import { Book } from "../literature/Book.ts";
 export type WriterGQL = {
     id: string,
     name: string,
-    surname: string | null,
-    image: string | null,
+    surname?: string,
+    image?: string,
     books: Book[],
 }
 
