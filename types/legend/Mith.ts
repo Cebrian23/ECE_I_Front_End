276c9ca9b8ff2aa0f39b2a89e4506bed4e693b61
Miguel <miguelangel5cebrian@gmail.com>
2025-10-17 19:10:35 +0200

    Actualización del tipo Mith

--- types/legend/Mith.ts
+++ types/legend/Mith.ts
@@ -1,21 +1,21 @@
 export type MithGQL = {
     id: string,
     name: string,
-    talked_about_in_song: {
+    talked_about_in_song?: {
         id: string,
         name: string,
-        cover: string
+        cover?: string
         album_in: {
             id: string,
             name: string,
-            cover: string,
+            cover?: string,
             year_of_publish: number,
         }
     }[],
-    talked_about_in_album: {
+    talked_about_in_album?: {
         id: string,
         name: string,
-        cover: string,
+        cover?: string,
         year_of_publish: number,
     }[],
 }
