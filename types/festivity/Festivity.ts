778d0aff55a7f9ac40e7d67741d37e64b02f3178
Miguel <miguelangel5cebrian@gmail.com>
2025-10-17 19:11:13 +0200

    Actualización del tipo Festivity

--- types/festivity/Festivity.ts
+++ types/festivity/Festivity.ts
@@ -7,21 +7,21 @@ export type FestivityGQL = {
         month: string,
         day: number,
     },
-    talked_about_in_song: {
+    talked_about_in_song?: {
         id: string,
         name: string,
-        cover: string
+        cover?: string
         album_in: {
             id: string,
             name: string,
-            cover: string,
+            cover?: string,
             year_of_publish: number,
         }
     }[],
-    talked_about_in_album: {
+    talked_about_in_album?: {
         id: string,
         name: string,
-        cover: string,
+        cover?: string,
         year_of_publish: number,
     }[],
 }
