964ea2338eaaa4520a145017d335622728d0c978
Miguel <miguelangel5cebrian@gmail.com>
2025-10-17 19:09:58 +0200

    Actualización del tipo Legend

--- types/legend/Legend.ts
+++ types/legend/Legend.ts
@@ -2,21 +2,21 @@
 export type LegendGQL = {
     id: string,
     name: string,
-    talked_about_in_song: {
+    talked_about_in_song?: {
         id: string,
         name: string,
-        cover: string
+        cover?: string
         album_in: {
             id: string,
             name: string,
-            cover: string,
+            cover?: string,
             year_of_publish: number,
         }
     }[],
-    talked_about_in_album: {
+    talked_about_in_album?: {
         id: string,
         name: string,
-        cover: string,
+        cover?: string,
         year_of_publish: number,
     }[],
 }
