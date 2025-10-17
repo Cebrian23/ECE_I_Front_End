f90e526ddbd25f83d23496f1c9b2b682ce17d03a
Miguel <miguelangel5cebrian@gmail.com>
2025-10-17 14:07:50 +0200

    Actualización del tipo Person

--- types/history/Person.ts
+++ types/history/Person.ts
@@ -44,10 +44,11 @@ export type PersonGQL = {
     talked_about_in_song: {
         id: string,
         name: string,
-        cover: string,
+        cover: string
         album_in: {
             id: string,
             name: string,
+            cover: string,
             year_of_publish: number,
         }
     }[],
