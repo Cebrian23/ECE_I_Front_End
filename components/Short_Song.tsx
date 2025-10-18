f5de532297510de3605434ebd753ce882e3f38f6
Miguel <miguelangel5cebrian@gmail.com>
2025-10-18 04:05:01 +0200

    Inserción del visualizador de un fragmento de canción

---
+++ components/Short_Song.tsx
@@ -0,0 +1,20 @@
+import { Song_Short } from "../types/music/Song.ts";
+
+type Data = {
+    song: Song_Short,
+}
+
+const Short_Song = (props: Data) => {
+    const song = props.song;
+
+    return(
+        <div class="short_block">
+            <img src={song.cover}/>
+            <p><a href={`/song/${song.id}`} class="a1">{song.name}</a></p>
+            <i><a href={`/album/${song.album_in.id}`} class="a1">{song.album_in.name + " (" + song.album_in.year_of_publish + ")"}</a></i>
+            <p><a href={`/band/${song.album_in.creator.id}`} class="a1">{song.album_in.creator.name}</a></p>
+        </div>
+    );
+}
+
+export default Short_Song;
