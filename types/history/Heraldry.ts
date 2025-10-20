f363cac50f19b5a09d0246190da1d29f651262de
Miguel <miguelangel5cebrian@gmail.com>
2025-10-20 02:55:43 +0200

    Actualización del tipo Heraldry

--- types/history/Heraldry.ts
+++ types/history/Heraldry.ts
@@ -1,32 +1,12 @@
+import { Album_Short } from "../music/Album.ts";
+import { Song_Short } from "../music/Song.ts";
+
 export type HeraldryGQL = {
     id: string,
     name: string,
     image?: string,
-    talked_about_in_song?: {
-        id: string,
-        name: string,
-        cover?: string
-        album_in: {
-            id: string,
-            name: string,
-            cover?: string,
-            year_of_publish: number,
-            creator: {
-                id: string,
-                name: string,
-            }
-        }
-    }[],
-    talked_about_in_album?: {
-        id: string,
-        name: string,
-        cover?: string,
-        year_of_publish: number,
-        creator: {
-            id: string,
-            name: string,
-        }
-    }[],
+    talked_about_in_song?: Song_Short[],
+    talked_about_in_album?: Album_Short[],
 }
 
 export type Heraldry = {
