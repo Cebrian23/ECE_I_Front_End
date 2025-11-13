d6e8b2fd6ab0075564ec898d0bb5d8b60f1f60d2
Miguel <miguelangel5cebrian@gmail.com>
2025-11-13 14:29:44 +0100

    Actualización del índice

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,20 @@
 const Page = () => {
   return (
-    <div>
+    <div class="index">
+      <h1>
+        Historia, Cultura, Literatura y Metal
+      </h1>
+      <h2>
+        Bienvenid@ a esta página web.
+      </h2>
+      <h3>
+        Esta herramienta, dependiendo de los filtros que se seleccionen,
+        te recomendará una serie de canciones y álbumes de rock & roll y heavy metal
+        que abordan una temática histórica, literaria o cultural.
+        <br/>
+        En la cabecera de esta página web se encuentran las diferentes
+        temáticas sobre las cuales se pueden aplicar los filtros
+      </h3>
     </div>
   );
 }
