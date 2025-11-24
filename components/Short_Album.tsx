2eab94d5103639d94a661b038bbb9101510c30d4
Miguel <miguelangel5cebrian@gmail.com>
2025-11-25 00:18:32 +0100

    Actualización del componente Short_Album

--- components/Short_Album.tsx
+++ components/Short_Album.tsx
@@ -10,8 +10,9 @@ const Short_Album = (props: Data) => {
     return(
         <div class="short_block">
             <image src={album.cover} width={250} height={350}/>
-            <br/>
-            <i><a href={`/album/${album.id}`} class="a1">{album.name + " (" + album.year_of_publish + ")"}</a></i>
+            <p>
+                <i><a href={`/album/${album.id}`} class="a1">{album.name + " (" + album.year_of_publish + ")"}</a></i>
+            </p>
             <p><a href={`/band/${album.creator.id}`} class="a1">{album.creator.name}</a></p>
         </div>
     );
