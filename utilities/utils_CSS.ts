a2efa12869fc2f0baef4b0cc79ce6ab81c7b56fc
Miguel <miguelangel5cebrian@gmail.com>
2025-11-24 20:05:52 +0100

    Actualización de las utilities

--- utilities/utils_CSS.ts
+++ utilities/utils_CSS.ts
@@ -9,7 +9,7 @@ import { Song, Song_Short } from "../types/music/Song.ts";
  * @param data Es el array de datos que hay que mostrar al usuario
  * @returns Devuelve el estilo que se devería usar a la hora de mostrar las canciones, albumes o libros
  */
-export const Class_Selector = (data: Song[] | Song_Short[] | Album_Shorter[] | Album_Short[] | BookGQL[]): string => {
+export const Class_Selector = (data: Song[] | Song_Short[] | Album_Shorter[] | Album_Short[] | BookGQL[], ind_page?: boolean): string => {
     if(data.length === 4 || data.length % 4 === 0){
         //console.log(4);
         return "group4";
@@ -23,8 +23,14 @@ export const Class_Selector = (data: Song[] | Song_Short[] | Album_Shorter[] | A
         return "group2";
     }
     else if(data.length === 1){
-        //console.log(1);
-        return "group1";
+        if(ind_page === true){
+            //console.log("ind_page");
+            return "group1_ind_page"
+        }
+        else if(!ind_page || ind_page === false){
+            //console.log(1);
+            return "group1";
+        }
     }
 
     //console.log("default");
