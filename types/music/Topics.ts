1ad791758b86957f6a01bc47b87982247f3621fd
Miguel <miguelangel5cebrian@gmail.com>
2025-11-29 00:59:54 +0100

    Actualización del tipo 'Topics'

--- types/music/Topics.ts
+++ types/music/Topics.ts
@@ -8,6 +8,47 @@ import { Festivity } from "../festivity/Festivity.ts";
 import { Mith } from "../legend/Mith.ts";
 import { Monument } from "../history/Monument.ts";
 
+export type Topics_Short = {
+    events: {
+        id: string,
+        name: string,
+    }[],
+    organizations: {
+        id: string,
+        name: string,
+    }[],
+    people: {
+        id: string,
+        name: string,
+        surname?: string,
+        country_from: string,
+    }[],
+    books: {
+        id: string,
+        title: string,
+    }[],
+    heraldries: {
+        id: string,
+        name: string,
+    }[],
+    legends: {
+        id: string,
+        name: string,
+    }[],
+    miths: {
+        id: string,
+        name: string,
+    }[],
+    festivities: {
+        id: string,
+        name: string,
+    }[],
+    monuments: {
+        id: string,
+        name: string,
+    }[],
+}
+
 export type Topics = {
     events?: Event[],
     organizations?: Organization[],
