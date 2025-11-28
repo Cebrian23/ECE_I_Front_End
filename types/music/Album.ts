c4e1915748cfdeb4d7f92b68028c95813643df3d
Miguel <miguelangel5cebrian@gmail.com>
2025-11-29 00:59:20 +0100

    Actualización del tipo 'Album'

--- types/music/Album.ts
+++ types/music/Album.ts
@@ -1,6 +1,6 @@
 import { Band_Short } from "./Band.ts";
 import { Song } from "./Song.ts";
-import { Topics } from "./Topics.ts";
+import { Topics, Topics_Short } from "./Topics.ts";
 
 export type AlbumGQL = {
     id: string,
@@ -15,46 +15,7 @@ export type AlbumGQL = {
         id: string,
         name: string,
     }[],
-    talk_about?: {
-        events: {
-            id: string,
-            name: string,
-        }[],
-        organizations: {
-            id: string,
-            name: string,
-        }[],
-        people: {
-            id: string,
-            name: string,
-            surname?: string,
-            contry_from: string,
-        }[],
-        books: {
-            id: string,
-            title: string,
-        }[],
-        heraldries: {
-            id: string,
-            name: string,
-        }[],
-        legends: {
-            id: string,
-            name: string,
-        }[],
-        miths: {
-            id: string,
-            name: string,
-        }[],
-        festivities: {
-            id: string,
-            name: string,
-        }[],
-        monuments: {
-            id: string,
-            name: string,
-        }[],
-    },
+    talk_about?: Topics_Short,
     conceptual_album: boolean,
 }
 
